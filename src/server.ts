5a2aa7ea597bf8b22f4d0ff3440c69de44e5204f
EmeTeBe <bernimarozzi@gmail.com>
2026-01-29 13:58:29 -0300

    add morgan for logging in server setup

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 import { corsConfig } from "./config/cors";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(cors(corsConfig));
 
 // Body parser
 app.use(express.json());
+// Logging
+app.use(morgan("dev"));
 
 // Middleware
 app.use("/api/projects", projectRoutes);
