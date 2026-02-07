d57a60eef10e2cf4ec2c87e3dcdff3b140b0f5c5
EmeTeBe <bernimarozzi@gmail.com>
2026-02-07 10:04:27 -0300

    add JWT generation utility and update login response in AuthController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,6 +4,7 @@ import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
+import { generateJWT } from "../utils/jwt";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -88,7 +89,10 @@ export class AuthController {
         const error = new Error("Password incorrecto");
         return res.status(401).json({ error: error.message });
       }
-      res.send("Autenticado...");
+
+      const jwt = generateJWT({ id: user._id });
+
+      res.send(jwt);
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
